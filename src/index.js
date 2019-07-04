c322b61ba7f74d1730eeab4dc7889b97751859d0
dngoyb <dngoyb@gmail.com>
2019-07-04 12:26:57 +0200

    adding Content component

--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const Header = (props) => {
    )
 }
 
+const Content = (props) => {
+   return (
+      <div>
+         <p>{props.part1} {props.exercises1}</p>
+         <p>{props.part2} {props.exercises2}</p>
+         <p>{props.part2} {props.exercises3}</p>
+      </div>
+   )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -21,15 +31,9 @@ const App = () => {
   return (
     <div>
       <Header course = {course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content part1={part1} exercises1={exercises1}/>
+      <Content part1={part2} exercises1={exercises2}/>
+      <Content part1={part1} exercises1={exercises3}/>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
